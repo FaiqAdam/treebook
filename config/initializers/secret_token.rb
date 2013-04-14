6fc168dfb450f553654aefb84ae108ba3c10f98c
Faiq Adam <faiq.adam@gmail.com>
2013-04-14 13:09:09 +0530

    Create a rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siteex::Application.config.secret_token = 'd8ca697804ab27aa87c9140c3498880ea7db3cce263e1d498060068ae5ffa61deafe79c934fd49bec90b6c063b6749694d964109a72352c41a531a5336fa3ec6'
